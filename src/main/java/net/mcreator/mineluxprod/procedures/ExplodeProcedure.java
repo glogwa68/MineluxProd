6826e9c957594afbb69b869e90afb161bf0cc6ba
darren.rapetti@hotmail.fr <Studio@DESKTOP-9T2IA9H>
2022-04-21 00:55:33 +0200

    commit

---
+++ src/main/java/net/mcreator/mineluxprod/procedures/ExplodeProcedure.java
@@ -0,0 +1,59 @@
+package net.mcreator.mineluxprod.procedures;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+
+import net.minecraft.world.IWorld;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.mineluxprod.MineluxprodMod;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class ExplodeProcedure {
+	@Mod.EventBusSubscriber
+	private static class GlobalTrigger {
+		@SubscribeEvent
+		public static void onRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
+			PlayerEntity entity = event.getPlayer();
+			if (event.getHand() != entity.getActiveHand()) {
+				return;
+			}
+			double i = event.getPos().getX();
+			double j = event.getPos().getY();
+			double k = event.getPos().getZ();
+			IWorld world = event.getWorld();
+			BlockState state = world.getBlockState(event.getPos());
+			Map<String, Object> dependencies = new HashMap<>();
+			dependencies.put("x", i);
+			dependencies.put("y", j);
+			dependencies.put("z", k);
+			dependencies.put("world", world);
+			dependencies.put("entity", entity);
+			dependencies.put("direction", event.getFace());
+			dependencies.put("blockstate", state);
+			dependencies.put("event", event);
+			executeProcedure(dependencies);
+		}
+	}
+
+	public static boolean executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				MineluxprodMod.LOGGER.warn("Failed to load dependency entity for procedure Explode!");
+			return false;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		if (((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY).getItem() == Blocks.TORCH.asItem()) {
+			return true;
+		}
+		return false;
+	}
+}
