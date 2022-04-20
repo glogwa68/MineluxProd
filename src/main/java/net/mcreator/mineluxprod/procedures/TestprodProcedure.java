4210a4e8d3be02570b37c0a54b99a4298ac105b3
clerc.antoine53500@gmail.com <clerc.antoine53500@gmail.com>
2022-04-21 00:08:36 +0200

    adding Procédure "Testprod" vierge.

---
+++ src/main/java/net/mcreator/mineluxprod/procedures/TestprodProcedure.java
@@ -0,0 +1,10 @@
+package net.mcreator.mineluxprod.procedures;
+
+import java.util.Map;
+
+public class TestprodProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+
+	}
+}
