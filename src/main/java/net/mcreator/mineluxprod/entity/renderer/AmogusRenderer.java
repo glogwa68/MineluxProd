57c105f52980cef909f0ec18d6f841e3ca93e07e
clerc.antoine53500@gmail.com <clerc.antoine53500@gmail.com>
2022-04-21 00:57:06 +0200

    adding amogus

---
+++ src/main/java/net/mcreator/mineluxprod/entity/renderer/AmogusRenderer.java
@@ -0,0 +1,37 @@
+
+package net.mcreator.mineluxprod.entity.renderer;
+
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.entity.Entity;
+import net.minecraft.client.renderer.entity.model.BipedModel;
+import net.minecraft.client.renderer.entity.layers.BipedArmorLayer;
+import net.minecraft.client.renderer.entity.BipedRenderer;
+
+import net.mcreator.mineluxprod.entity.AmogusEntity;
+
+@OnlyIn(Dist.CLIENT)
+public class AmogusRenderer {
+	public static class ModelRegisterHandler {
+		@SubscribeEvent
+		@OnlyIn(Dist.CLIENT)
+		public void registerModels(ModelRegistryEvent event) {
+			RenderingRegistry.registerEntityRenderingHandler(AmogusEntity.entity, renderManager -> {
+				BipedRenderer customRender = new BipedRenderer(renderManager, new BipedModel(0), 0.5f) {
+					@Override
+					public ResourceLocation getEntityTexture(Entity entity) {
+						return new ResourceLocation("mineluxprod:textures/c42f8eaf8ed2b22452da2f7dbf560fe77b3cf2f42410c15312f01d63ab60070c.png");
+					}
+				};
+				customRender.addLayer(new BipedArmorLayer(customRender, new BipedModel(0.5f), new BipedModel(1)));
+
+				return customRender;
+			});
+		}
+	}
+}
